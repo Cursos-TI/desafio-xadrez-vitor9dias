aa15b795b0c815ebc7981a011b21f0a31cd860a2
vitor9dias <vitorh9d@gmail.com>
2025-04-21 02:43:01 +0000

    adição do loop aninhado e recursiva

--- xadrez.c
+++ xadrez.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+
+void recursivotorre(int torre){
+    if(torre > 0){
+        printf("Direita\n");
+        recursivotorre(torre-1);
+    }
+}
+
+void recursivobispo(int bispo){
+    if(bispo >0 ){
+        printf("Cima, Direita\n");
+        recursivobispo(bispo-1);
+    }
+}
+
+void recursivorainha(int rainha){
+    if(rainha > 0){
+        printf("Esquerda\n");
+        recursivorainha(rainha-1);
+    }
+}
+
+
  int main(){
-int a=1 , i=1, j=1;
+int a=1 , i=1, j=1, quantidadetorre=5, quantidadebispo=5, quantidaderainha=8;
 
     printf("**Movimentação da Torre**\n");
         while(i<=5){                    // repetição da movimentação da Torre
@@ -31,9 +54,37 @@ int a=1 , i=1, j=1;
 
         printf("Esquerda\n");
     }printf("\n");
+    printf("\n");
+    printf("\n");
+
+    printf("** Movimentação da Torre**\n"); // Mudando a movimentação da torre de loop simples para recursiva
+     recursivotorre(quantidadetorre);
+    printf("\n");
 
 
+    printf("**Moviment da Torre**\n"); // /Mudando a movimentação para recursiva
+     recursivobispo(quantidadebispo);
+     printf("\n");
 
+     printf("**Movimentação da Rainha**"); //Mudando a movimentação para recursiva
+     recursivorainha(quantidaderainha);
+     printf("\n");
+ 
+printf("**Movimentação do Cavalo**\n"); //uso de loop aninhado 
+for(int a=0; a<=0;a++){
+    for(int b=1; b<=2; b++){
+        printf("Baixo\n");
+    }printf("Esquerda\n");
+} printf("\n");
+
+    printf("**Movimentação Bispo**\n"); // uso de loop aninhado 
+    for(int a=1; a<=5; a++){
+        for(int b= 5; b<=5; b++){
+            printf("Direita\n");
+        }printf("Cima\n");
+
+    }
+    
 
 
     return 0;
