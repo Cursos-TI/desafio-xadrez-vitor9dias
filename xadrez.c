a2d82e42076f24c37d140226799425f7a920f68e
vitor9dias <vitorh9d@gmail.com>
2025-04-21 01:18:35 +0000

    adicionado loop aninhado for e while

--- xadrez.c
+++ xadrez.c
@@ -1,23 +1,37 @@
 #include <stdio.h>
  int main(){
-int a=1 , i=1;
+int a=1 , i=1, j=1;
 
     printf("**Movimentação da Torre**\n");
         while(i<=5){                    // repetição da movimentação da Torre
     printf("Direita\n");
         i++;
-    }
+    } printf("\n");
+
     printf ("**Moivimentação do Bispo**\n");
         do{ (a<=5);                    // repetição do movimento do Bispo
       printf("Cima e Direita \n");
         // repetição do movimento do Bispo
         a++;
         }  while (a<=5);
+        printf("\n");
         
     printf("**Movimentação da rainha**\n");
         for(i=1; i<=8; i++){          // repetiçao do movimento da Rainha
         printf("Esquerda\n");
-        }
+        }printf("\n");
+
+
+     printf("**Movimentação do Cavalo**\n");
+        for(int c = 1; c<=1; c++){   //repetição do esquerda uma vez
+            while(j<=2){             //repetição do baixo duas vezes 
+                j++;
+            printf("Baixo\n");
+            }
+
+        printf("Esquerda\n");
+    }printf("\n");
+
 
 
 
